0e85e187f4990ed1b8158c75ef609fe552db55c7
cazsky <lolcchz@gmail.com>
2025-01-04 17:16:40 +0800

    Added add score on click

---
+++ Scripts/game.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+# Scene nodes
+@onready var speed_label: Label = $Control/Stats/Speed
+@onready var per_bounce_label: Label = $Control/Stats/PerBounce
+@onready var score_label: Label = $Control/Stats/Score
+
+# Init variables
+var score: int = 0
+var add: int = 1
+var add_per_bounce: int = 0
+var combo: int = 0
+var speed: int = 0 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	score_label.text = str("Score: ", score)
+	per_bounce_label.text = str("PerBounce: ", add_per_bounce)
+	speed_label.text = str("Speed: ", speed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	score_label.text = str("Score: ", score)
+
+
+func _on_click_pressed() -> void:
+	score += add
