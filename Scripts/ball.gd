7af1c02bde39ff2569f844299e80ee79f98e4947
cazsky <lolcchz@gmail.com>
2025-01-07 01:36:09 +0800

    Ball now srarts moving on click

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -3,26 +3,21 @@ extends CharacterBody2D
 @export var speed: float = 600.0 
 
 var current_trail: Trail
+var is_moving: bool = false
 
 @warning_ignore("unused_signal")
 signal bounce
 
 func _ready() -> void:
-	# Generate a random direction
-	# TAU is 2PI, gives a random angle in radians
-	# Use TAU for a full 360 degrees
-	var angle = randf() * TAU 
-	# cos(angle) gives the distance along the x-axis and sin(angle) for y-axis
-	var direction = Vector2(cos(angle), sin(angle)).normalized()
-	velocity = direction * speed
-	
+	# Set initial velocity to ZERO so the ball doesnt move
+	velocity = Vector2.ZERO
 	make_trail()
 
 
 func _physics_process(delta: float) -> void:
-	
-	detect_bounce()
-	move_and_slide()
+	if is_moving:
+		detect_bounce()
+		move_and_slide()
 
 
 func make_trail() -> void:
@@ -38,6 +33,14 @@ func detect_bounce() -> void:
 		# Connect signal to main game to add score whenever ball bounces
 		emit_signal("bounce")
 
-
+# We want for the ball to move a certain distance whenever the screen is tapped.
 func _on_button_pressed() -> void:
-	pass # Replace with function body.
+	if not is_moving:
+		# Generate a random direction
+		# TAU is 2PI, gives a random angle in radians
+		# Use TAU for a full 360 degrees
+		var angle = randf() * TAU 
+		# cos(angle) gives the distance along the x-axis and sin(angle) for y-axis
+		var direction = Vector2(cos(angle), sin(angle)).normalized()
+		velocity = direction * speed
+		is_moving = true
