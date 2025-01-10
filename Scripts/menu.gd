ea3250d2ae66dc5e853ef36e73c61cc5b5abdbe9
cazsky <lolcchz@gmail.com>
2025-01-11 01:53:53 +0800

    Refining Tweens

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -5,6 +5,9 @@ extends Node2D
 @onready var power_button: Button = $Control/TabContainer/Upgrades/GridContainer/Power
 @onready var bounciness_button: Button = $Control/TabContainer/Upgrades/GridContainer/Bounciness
 @onready var score_button: Button = $Control/TabContainer/Upgrades/GridContainer/Score
+@onready var popup: Button = $Control/Popup
+
+const MENU_TIME: float = 0.15
 
 # Ball price upgrade multiplers
 var speed_upgrade_price_multiplier: float = 1.2
@@ -25,9 +28,13 @@ func close_menu() -> void:
 	
 func open_menu() -> void:
 	var tw := get_tree().create_tween()
-	tw.tween_property(self, "global_position", Vector2(0,300), 0.2)
+	tw.set_parallel()
+	# Moving the whole menu baord up
+	tw.tween_property(self, "global_position", Vector2(0,300), MENU_TIME).set_trans(tw.TRANS_SINE).set_ease(tw.EASE_OUT)
+	# Changing size of the pop up button
+	tw.tween_property(popup, "size", Vector2(50,popup.size.y), MENU_TIME)
+	tw.tween_property(popup, "position", Vector2(600,0), MENU_TIME).set_trans(tw.TRANS_SINE).set_ease(tw.EASE_OUT)
 	await tw.finished
-
 
 func _on_texture_button_pressed() -> void:
 	if is_menu_open:
