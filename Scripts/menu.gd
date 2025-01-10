40297768804b4b071b27548ff3b58fb3b996d2da
cazsky <lolcchz@gmail.com>
2025-01-11 02:10:58 +0800

    Arrow moves with menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var bounciness_button: Button = $Control/TabContainer/Upgrades/GridContainer/Bounciness
 @onready var score_button: Button = $Control/TabContainer/Upgrades/GridContainer/Score
 @onready var popup: Button = $Control/Popup
+@onready var arrow: Sprite2D = $Control/Arrow
 
 const MENU_TIME: float = 0.15
 
@@ -16,25 +17,28 @@ var is_menu_open: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
+	
 func close_menu() -> void:
 	pass
 	
 func open_menu() -> void:
+	arrow.flip_v = true
 	var tw := get_tree().create_tween()
 	tw.set_parallel()
 	# Moving the whole menu baord up
 	tw.tween_property(self, "global_position", Vector2(0,300), MENU_TIME).set_trans(tw.TRANS_SINE).set_ease(tw.EASE_OUT)
 	# Changing size of the pop up button
-	tw.tween_property(popup, "size", Vector2(50,popup.size.y), MENU_TIME)
-	tw.tween_property(popup, "position", Vector2(600,0), MENU_TIME).set_trans(tw.TRANS_SINE).set_ease(tw.EASE_OUT)
+	# This is crazy spaghetti
+	tw.tween_property(popup, "global_position", Vector2(get_viewport().size.x - 45,300), MENU_TIME).set_trans(tw.TRANS_SINE).set_ease(tw.EASE_OUT)
+	tw.tween_property(popup, "size", Vector2(1000,popup.size.y), MENU_TIME)
 	await tw.finished
+	
 
 func _on_texture_button_pressed() -> void:
 	if is_menu_open:
