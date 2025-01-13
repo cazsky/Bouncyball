96aea061f19a6b0be25fcc834716686bab0d0f48
cazsky <lolcchz@gmail.com>
2025-01-14 00:43:56 +0800

    Updated labels

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -13,6 +13,7 @@ var current_trail: Trail
 var is_moving: bool = false
 var can_push: bool = true
 var damping: float = 0.98
+var max_velocity: float = current_speed
 
 @warning_ignore("unused_signal")
 signal bounce
@@ -35,7 +36,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	# Cant set to minimum 0 or the ball will just stop i guess
-	velocity = velocity.clampf(-10000, current_speed)
+	velocity = velocity.clampf(-10000, max_velocity)
 
 	# Stop movement when velocity is near zero
 	if velocity.length() < 10:
--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
 @onready var speed_button: Button = $Control/TabContainer/Upgrades/GridContainer/Speed
-@onready var friction_button: Button = $Control/TabContainer/Upgrades/GridContainer/Friction
+@onready var velocity_button: Button = $Control/TabContainer/Upgrades/GridContainer/Velocity
 @onready var xp_button: Button = $Control/TabContainer/Upgrades/GridContainer/XPGain
 @onready var bounciness_button: Button = $Control/TabContainer/Upgrades/GridContainer/Bounciness
 @onready var score_button: Button = $Control/TabContainer/Upgrades/GridContainer/Score
+
+@onready var speed_price_label: Label = $Control/TabContainer/Upgrades/VBoxContainer/SpeedPrice
+@onready var velocity_price_label: Label = $Control/TabContainer/Upgrades/VBoxContainer/VelocityPrice
+@onready var xp_price_label: Label = $Control/TabContainer/Upgrades/VBoxContainer/XPPrice
+@onready var bounciness_price_label: Label = $Control/TabContainer/Upgrades/VBoxContainer/BouncinessPrice
+@onready var score_price_label: Label = $Control/TabContainer/Upgrades/VBoxContainer/ScorePrice
+
 @onready var popup: Button = $Control/Popup
 @onready var arrow: Sprite2D = $Control/Arrow
 @onready var ball: CharacterBody2D = $"../Ball"
@@ -14,7 +21,8 @@ extends Node2D
 
 # Base upgrade price
 const BASE_SPEED_PRICE: int = 5
-const BASE_FRICTION_PRICE: int = 10
+const BASE_VELOCITY_PRICE: int = 10
+const BASE_XP_PRICE: int = 15
 const BASE_BOUNCINESS_PRICE: int = 30
 const BASE_SCORE_PRICE: int = 50
 
@@ -24,31 +32,34 @@ const MENU_TIME: float = 0.15
 
 # Ball price upgrade multiplers
 var speed_upgrade_price_multiplier: float = 1.2
-var friction_upgrade_price_multiplier: float = 1.2
+var velocity_upgrade_price_multiplier: float = 1.2
 var bounciness_upgrade_price_multiplier: float = 1.3
 
 # Ball stats upgrades multipliers
 var speed_upgrade_stat_multiplier: float = 1.05
-var friction_upgrade_stat_multiplier: float = 0.95
+var velocity_upgrade_stat_multiplier: float = 0.95
 var bounciness_upgrade_stat_multiplier: float = 1.2
 
 # Initialise vars
 var is_menu_open: bool = false
 var base_bounciness: float = 1.0
 var bounciness: float = base_bounciness
-var friction_price: int = BASE_FRICTION_PRICE
+var velocity_price: int = BASE_VELOCITY_PRICE
+var speed_price: int = BASE_SPEED_PRICE
+var xp_price: int = BASE_XP_PRICE
 var bounciness_price: int = BASE_BOUNCINESS_PRICE
 var score_price: int = BASE_SCORE_PRICE
-var speed_price: int = BASE_SPEED_PRICE
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print_debug(ball)
 	# Connect the ball bounce signal
 	ball.bounce.connect(_on_ball_bounce)
-	pass
-
+	update_price(speed_price_label,speed_price)
+	update_price(velocity_price_label, velocity_price)
+	update_price(xp_price_label, xp_price)
+	update_price(bounciness_price_label, bounciness_price)
+	update_price(score_price_label, score_price)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -89,13 +100,11 @@ func _on_texture_button_pressed() -> void:
 # Speed upgrade
 func _on_speed_pressed() -> void:
 	ball.current_speed *= speed_upgrade_stat_multiplier
+	ball.velocity *= speed_upgrade_stat_multiplier
 	speed_stat_label.text = str("Speed: ", roundf(ball.current_speed))
 	speed_price *= speed_upgrade_price_multiplier
+	update_price(speed_price_label, speed_price)
 
-# Friction upgrade
-func _on_friction_pressed() -> void:
-	ball.friction *= friction_upgrade_stat_multiplier
-	friction_price *= friction_upgrade_price_multiplier
 
 # Bounciness effect on collide
 func _on_ball_bounce() -> void:
@@ -109,3 +118,8 @@ func _on_bounciness_pressed() -> void:
 # Function to update price values on labels easier
 func update_price(label: Label, price: int) -> void:
 	label.text = str("Current Price: \n", price)
+
+# Velocity upgrade
+func _on_velocity_pressed() -> void:
+	ball.max_velocity *= velocity_upgrade_stat_multiplier
+	velocity_price *= velocity_upgrade_price_multiplier
