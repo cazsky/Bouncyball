bc755516bd6a4d979a2bf897b3afef3a4495a34b
cazsky <lolcchz@gmail.com>
2025-01-25 22:32:21 +0800

    Added level up text animation

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,12 +5,13 @@ extends Node2D
 @onready var velocity_label: Label = $Control/Stats/Velocity
 @onready var ball: CharacterBody2D = $Ball
 @onready var xp_bar: TextureProgressBar = $Control/xp_bar
+#@onready var level_up_text: Label = $Control/xp_bar/level_up_text
 
 # Init variables
 var score: float = 0
 var add: float = 1
 var combo: int = 0
-var xp_gain: float = 5.0
+var xp_gain: float = 20.0
 
 func _init() -> void:
 	# Randomise seed to start the game launching ball in random direction
@@ -34,3 +35,20 @@ func _on_ball_bounce() -> void:
 
 func _on_ball_levelled_up() -> void:
 	score += 10
+	display_level_up_text()
+
+
+func display_level_up_text() -> void:
+	var level_up_text = preload("res://Scenes/level_up_text.tscn").instantiate()
+	level_up_text.modulate.a = 0
+	xp_bar.add_child(level_up_text)
+	# Get center of xp_bar
+	var xp_bar_center: Vector2 = xp_bar.global_position + xp_bar.get_rect().size * 0.5
+	# Get center of level_up_text
+	var level_up_text_center: Vector2 = level_up_text.get_rect().size * 0.5
+	# Animate tween
+	var target_position: Vector2 = Vector2(xp_bar_center.x - level_up_text_center.x, xp_bar.global_position.y - 50)
+	var tw := get_tree().create_tween()
+	tw.set_parallel()
+	tw.tween_property(level_up_text, "modulate:a", 1, 0.2)
+	tw.tween_property(level_up_text, "global_position", target_position, 0.4)
