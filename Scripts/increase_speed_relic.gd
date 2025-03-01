9773c7abec7cc168db9368137da45c2a061c12ac
cazsky <86903948+cazsky@users.noreply.github.com>
2025-03-02 00:16:25 +0800

    Added levels

--- Scripts/increase_speed_relic.gd
+++ Scripts/increase_speed_relic.gd
@@ -1,8 +1,11 @@
 extends Relic
 
+const BASE_COST: float = 10
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	relic_name = "wings_of_hermes"
 	cost_multiplier = 1.5
 	ball.current_speed *= stat_multiplier
+	cost = BASE_COST * cost_multiplier * level
--- Scripts/relic.gd
+++ Scripts/relic.gd
@@ -8,6 +8,7 @@ class_name Relic
 @export var cost_multiplier: float = 0
 @export var stat_multiplier: float = 1
 @export var relic_name: String
+@export var level: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +16,4 @@ func _ready() -> void:
 
 func _upgrade() -> void:
 	cost *= cost_multiplier
+	level += 1
