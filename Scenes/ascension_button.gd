a1dbee2c3b193225ab338302bbcb5e3998c7bf75
cazsky <86903948+cazsky@users.noreply.github.com>
2025-03-27 00:46:24 +0800

    Adding labels

--- Scenes/ascension_button.gd
+++ Scenes/ascension_button.gd
@@ -4,7 +4,7 @@ class_name AscensionButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	super()
 
 func update_label(stat: float, multiplier: float, price: float) -> void:
 	stat_label.text = str(snapped(stat, 0.01), " -> ", snapped(stat*multiplier, 0.01))
