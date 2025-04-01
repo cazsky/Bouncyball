f9b43c3f56ce94416d8564f97cc8e022eb39b4d2
cazsky <86903948+cazsky@users.noreply.github.com>
2025-04-01 22:09:26 +0800

    Removed comment

--- Scripts/perk_button.gd
+++ Scripts/perk_button.gd
@@ -11,12 +11,3 @@ func update_perk(current_stack: int, max_stacks: int, price: float) -> void:
 	stat_label.text = str(current_stack, " / ", max_stacks)
 	price_label.text = str("Current Price: ", "\n", price, " gems")
 	
-#func activate_perk(current_stack: int, max_stacks: int, price: float, stat) -> void:
-	#stat *= 2
-	#print_debug(stat)
-	#current_stack += 1
-	#update_perk(current_stack, max_stacks, price)
-	#await get_tree().create_timer(60).timeout
-	#stat /= 2
-	#current_stack -= 1
-	#update_perk(current_stack, max_stacks, price)
