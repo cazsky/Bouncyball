7cf21e39563531396aeebf9c1a3cb990c0b74bfe
cazsky <86903948+cazsky@users.noreply.github.com>
2025-04-10 16:21:58 +0800

    Save stats

--- Scripts/saveload.gd
+++ Scripts/saveload.gd
@@ -1,11 +1,21 @@
+class_name SavedGame
 extends Resource
 
+# Game stats
+@export var ball_position: Vector2
+@export var score: float
+@export var gems: float
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Ball stats
+@export var speed_upgrade_level: float
+@export var bounciness_upgrade_level: float
+@export var friction_upgrade_level: float
+@export var xp_upgrade_level: float
+@export var score_upgrade_level: float
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Double stacks
+@export var double_speed_stack: int
+@export var double_bounce_stack: int
+@export var double_xp_stack: int
+@export var double_score_stack: int
+@export var double_ball_stack: int
