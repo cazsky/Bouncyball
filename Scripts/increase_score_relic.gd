05b67a8f251620b4e87bb3776e4730f850c29c91
cazsky <86903948+cazsky@users.noreply.github.com>
2025-04-29 08:36:17 +0800

    New relic

---
+++ Scripts/increase_score_relic.gd
@@ -0,0 +1,28 @@
+class_name BallTalisman
+extends Relic
+
+const BASE_COST: float = 10
+const BASE_MULT: float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cost = BASE_COST * cost_multiplier * fmod(pow(level,2), 10)
+	
+	
+func _init() -> void:
+	relic_name = "ball_talisman"
+	cost_multiplier = 1.5
+	stat_multiplier = 1.05
+	relic_effect = "score_mult"
+	scene_path = "res://Scenes/relics/increase_score_relic.tscn"
+
+	
+func _activate_effect() -> void:
+	super()
+	menu.score_mult = menu.get(relic_effect) * stat_multiplier
+	print_debug("Effect score mult: ", menu.get(relic_effect))
+	
+func _upgrade() -> void:
+	super()
+	stat_multiplier = pow(BASE_MULT, self.level)
+	_activate_effect()
