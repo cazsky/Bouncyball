7684c845bec00671150bf8ce4a4cd702a244b053
cazsky <86903948+cazsky@users.noreply.github.com>
2025-05-04 00:06:02 +0800

    new relic

---
+++ Scripts/Relic Scripts/increase_bounce_relic.gd
@@ -0,0 +1,23 @@
+class_name BounceMania
+extends Relic
+
+const BASE_COST: float = 30
+const BASE_MULT: float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cost = BASE_COST * cost_multiplier * fmod(pow(level,2), 10)
+	
+	
+func _init() -> void:
+	relic_name = "ball_mania"
+	cost_multiplier = 1.5
+	stat_multiplier = 1.05
+	relic_effect = "ball.bounce"
+	scene_path = "res://Scenes/relics/increase_bounce_relic.tscn"
+
+	
+func _upgrade() -> void:
+	super()
+	stat_multiplier = pow(BASE_MULT, self.level)
+	_activate_effect()
