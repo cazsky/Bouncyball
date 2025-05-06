0086e82f2e11e82d7678eab569394abef711739d
cazsky <86903948+cazsky@users.noreply.github.com>
2025-05-07 00:08:50 +0800

    New relic prolly

---
+++ Scripts/Relic Scripts/increase_gem_gain_relic.gd
@@ -0,0 +1,26 @@
+class_name ShinyOrb
+extends Relic
+
+const BASE_COST: float = 30
+const BASE_MULT: float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cost = BASE_COST * cost_multiplier * fmod(pow(level,2), 10)
+	
+	
+func _init() -> void:
+	relic_name = "ball_mania"
+	cost_multiplier = 1.5
+	stat_multiplier = 1.05
+	relic_effect = "gem_gain_multiplier"
+	scene_path = "res://Scenes/relics/increase_bounce_relic.tscn"
+
+func _activate_effect() -> void:
+	menu = await wait_for_node("../../../../../../Menu")
+	menu.game.set(relic_effect, menu.game.get(relic_effect) * stat_multiplier)
+	
+func _upgrade() -> void:
+	super()
+	stat_multiplier = pow(BASE_MULT, self.level)
+	_activate_effect()
