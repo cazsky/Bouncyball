8dc8dddc0ca2de9b05cdeca3bf7bec59978a15f6
cazsky <86903948+cazsky@users.noreply.github.com>
2025-05-26 00:16:36 +0800

    time_left var

--- Scripts/saved_perk_data.gd
+++ Scripts/saved_perk_data.gd
@@ -5,23 +5,28 @@ class_name SavedPerkData
 @export var double_speed_stack: int = 0
 @export var double_speed_price: int = 200
 @export var double_speed_time: int = 5
+@export var double_speed_time_left: int = 0
 
 # XP perk
 @export var double_xp_stack: int = 0
 @export var double_xp_price: int = 500
 @export var double_xp_time: int = 30
+@export var double_xp_time_left: int = 0
 
 # Bounce perk
 @export var double_bounce_stack: int = 0
 @export var double_bounce_price: int = 1000
 @export var double_bounce_time: int = 30
+@export var double_bounce_time_left: int = 0
 
 # Score perk
 @export var double_score_stack: int = 0
 @export var double_score_price: int = 2000
 @export var double_score_time: int = 30
+@export var double_score_time_left: int = 0
 
 # Ball perk
 @export var double_ball_stack: int = 0
 @export var double_ball_price: int = 5000
 @export var double_ball_time: int = 30
+@export var double_ball_time_left: int = 0
