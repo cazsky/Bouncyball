ca21bdbeafa90f888eeeb9f90095a0765b7369d3
cazsky <86903948+cazsky@users.noreply.github.com>
2025-06-03 00:06:28 +0800

    double bounce

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -360,19 +360,21 @@ func _on_double_bounciness_pressed() -> void:
 	if game.gems >= double_bounce_price:
 		game.gems -= double_bounce_price
 		if double_bounce_stack < max_stacks:
-			double_bounce_stack += 1
-			bounciness = BASE_BOUNCINESS * bounce_mult * pow(2, double_bounce_stack)
-			double_bounce_button.update_perk(double_bounce_stack, max_stacks, double_bounce_price)
-			bounciness_button.update_label(bounciness, bounciness_upgrade_stat_multiplier, bounciness_price)
-			double_bounce_timer.start(double_bounce_time)
-			double_bounce_timer.is_active = true
-			await double_bounce_timer.timeout
-			double_bounce_timer.is_active = false
-			double_bounce_stack -= 1
-			bounciness = BASE_BOUNCINESS * bounce_mult * pow(2, double_bounce_stack)
-			double_bounce_button.update_perk(double_bounce_stack, max_stacks, double_bounce_price)
-			bounciness_button.update_label(bounciness, bounciness_upgrade_stat_multiplier, bounciness_price)
-
+			activate_double_bounciness_perk()
+			
+func activate_double_bounciness_perk() -> void:
+	double_bounce_stack += 1
+	bounciness = BASE_BOUNCINESS * bounce_mult * pow(2, double_bounce_stack)
+	double_bounce_button.update_perk(double_bounce_stack, max_stacks, double_bounce_price)
+	bounciness_button.update_label(bounciness, bounciness_upgrade_stat_multiplier, bounciness_price)
+	double_bounce_timer.start(double_bounce_time)
+	double_bounce_timer.is_active = true
+	await double_bounce_timer.timeout
+	double_bounce_timer.is_active = false
+	double_bounce_stack -= 1
+	bounciness = BASE_BOUNCINESS * bounce_mult * pow(2, double_bounce_stack)
+	double_bounce_button.update_perk(double_bounce_stack, max_stacks, double_bounce_price)
+	bounciness_button.update_label(bounciness, bounciness_upgrade_stat_multiplier, bounciness_price)
 
 func _on_double_ball_pressed() -> void:
 	if game.gems >= double_ball_price:
