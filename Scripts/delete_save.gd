16180fe03d208abecb7ff5cb3ac5d09aca809c5b
cazsky <86903948+cazsky@users.noreply.github.com>
2025-09-05 00:07:31 +0800

    I cant believe this worked

--- Scripts/delete_save.gd
+++ Scripts/delete_save.gd
@@ -2,8 +2,10 @@ extends Button
 const SAVE_GAME_PATH: String = "user://savegame.tres"
 
 @onready var menu = $"../../../../.."
-
+@onready var saver_loader: SaverLoader = $Utils/SaverLoader
 # Delete save
 func _on_pressed() -> void:
-	menu._reset_stats()
 	DirAccess.remove_absolute(SAVE_GAME_PATH)
+	menu._reset_stats()
+	menu.saver_loader.save_game()
+	
