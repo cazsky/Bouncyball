04bcffb364c720222577d414352e0acb62393a74
cazsky <86903948+cazsky@users.noreply.github.com>
2025-09-06 01:10:17 +0800

    Removed redudant line

--- Scripts/delete_save.gd
+++ Scripts/delete_save.gd
@@ -2,7 +2,7 @@ extends Button
 const SAVE_GAME_PATH: String = "user://savegame.tres"
 
 @onready var menu = $"../../../../.."
-@onready var saver_loader: SaverLoader = $Utils/SaverLoader
+
 # Delete save
 func _on_pressed() -> void:
 	DirAccess.remove_absolute(SAVE_GAME_PATH)
