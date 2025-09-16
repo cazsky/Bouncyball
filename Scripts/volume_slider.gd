a7ca604fbbd00d7f56024991460b09efcb4bcfb2
cazsky <86903948+cazsky@users.noreply.github.com>
2025-09-17 07:44:05 +0800

    volume slider even though theres no sound lol idiot

---
+++ Scripts/volume_slider.gd
@@ -0,0 +1,10 @@
+extends HSlider
+
+@export var master_volume: String = "Master"
+var master_volume_index: int = -1
+
+func _ready() -> void:
+	master_volume_index = AudioServer.get_bus_index(master_volume)
+	var db = AudioServer.get_bus_volume_db(master_volume_index)
+	self.value = db_to_linear(db)
+	
