f57e9ac382f839ffa0b24f8c8874c41ae5046ae8
cazsky <86903948+cazsky@users.noreply.github.com>
2025-09-22 00:34:56 +0800

    Figure this new relic out pls

---
+++ Scripts/Relic Scripts/decrease_speed_upgrade_price_relic.gd
@@ -0,0 +1,23 @@
+class_name Hustler
+extends Relic
+
+const BASE_COST: float = 30
+const BASE_MULT: float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cost = BASE_COST * cost_multiplier * fmod(pow(level,2), 10)
+	
+	
+func _init() -> void:
+	relic_name = "hustler"
+	cost_multiplier = 1.5
+	stat_multiplier = 1.05
+	relic_effect = "bounce_mult"
+	scene_path = "res://Scripts/Relic Scripts/decrease_speed_upgrade_price_relic.gd"
+
+	
+func _upgrade() -> void:
+	super()
+	stat_multiplier = pow(BASE_MULT, self.level)
+	_activate_effect()
