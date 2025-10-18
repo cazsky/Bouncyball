693670ac1ceb2a311cfeb4e72e4e13bb7a73fb7a
cazsky <86903948+cazsky@users.noreply.github.com>
2025-10-18 23:20:54 +0800

    new script

---
+++ Scripts/relic_container.gd
@@ -0,0 +1,15 @@
+extends VBoxContainer
+class_name Relic_Container
+
+@export var wrapper_class: String = "Control"
+
+func add_wrapped_child(child: Node) -> void:
+	var wrapper: Control
+	wrapper = ClassDB.instantiate(wrapper_class)
+	wrapper.name = "Control_Wrapper_%s" % child.name
+	
+	# Add child to control node
+	wrapper.add_child(child)
+	
+	# Add control node to grid container
+	add_child(wrapper)
