3c49c0e6ff883f59fa926fbe0042299966a7c9a1
cazsky <86903948+cazsky@users.noreply.github.com>
2025-11-18 01:07:06 +0800

    hopefully doesnt brick again

--- Scripts/relic.gd
+++ Scripts/relic.gd
@@ -2,9 +2,8 @@ extends Node2D
 class_name Relic
 
 @onready var menu: Node2D = $"../../../../../../../../Menu"
-#@export var ball_path: NodePath
-#@onready var ball: CharacterBody2D = get_node("NodePath")
 @onready var ball: CharacterBody2D = get_tree().current_scene.get_node("Ball")
+@onready var label: Label = $Label
 
 var cost: float = 0
 var cost_multiplier: float = 0
@@ -17,19 +16,19 @@ var relic_id: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	
+	await ready
 	
 func _upgrade() -> void:
 	cost *= cost_multiplier
 	level += 1
 
 func _activate_effect() -> void:
-	menu = await wait_for_node("../../../../../../../../Menu")
 	menu.set(relic_effect, menu.get(relic_effect) * stat_multiplier)
+	update_label()
 
 
 func _on_tree_entered() -> void:
+	await ready
 	_activate_effect()
 	
 func wait_for_node(path: NodePath) -> Node:
@@ -38,3 +37,6 @@ func wait_for_node(path: NodePath) -> Node:
 		await get_tree().process_frame
 		node = get_node_or_null(path)
 	return node
+	
+func update_label() -> void:
+	label.text = "%s: +%s%%" % [relic_effect, stat_multiplier]
