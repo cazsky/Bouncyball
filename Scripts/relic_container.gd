b3341656c32885c6f38392cdbbc8601e0652a573
cazsky <86903948+cazsky@users.noreply.github.com>
2025-11-24 01:38:59 +0800

    Fixed missing relics

--- Scripts/relic_container.gd
+++ Scripts/relic_container.gd
@@ -7,6 +7,7 @@ func add_wrapped_child(child: Node) -> void:
 	var wrapper: Control
 	wrapper = ClassDB.instantiate(wrapper_class)
 	wrapper.name = "Control_Wrapper_%s" % child.name
+	wrapper.set_custom_minimum_size(Vector2(0,50))
 	
 	# Add child to control node
 	wrapper.add_child(child)
