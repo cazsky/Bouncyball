79c47986c73560778451a6c2a20f3826ea0914e3
cazsky <86903948+cazsky@users.noreply.github.com>
2025-11-26 00:24:22 +0800

    laziness is a sin and im going to hell

--- Scenes/relic_upgrade_button.gd
+++ Scenes/relic_upgrade_button.gd
@@ -6,8 +6,7 @@ class_name RelicUpgradeButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label.text = label_text
-
+	pass
+	
 func update_label(stat: float, multiplier: float, price: float) -> void:
-	stat_label.text = str(snapped(stat, 0.01), " -> ", snapped(stat*multiplier, 0.01))
 	price_label.text = str("Current Price: \n", snapped(price,0.01), " score")
