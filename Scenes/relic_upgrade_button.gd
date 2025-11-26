0c35ef7a5fdf0c900bb9272fa182b255f446f33b
cazsky <86903948+cazsky@users.noreply.github.com>
2025-11-26 09:46:20 +0800

    update label

--- Scenes/relic_upgrade_button.gd
+++ Scenes/relic_upgrade_button.gd
@@ -3,10 +3,12 @@ class_name RelicUpgradeButton
 
 @onready var label: Label = $"../Label"
 @onready var price_label: Label = $"../PriceLabel"
+@onready var relic = $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 	
-func update_label(stat: float, multiplier: float, price: float) -> void:
+func update_label(price: float) -> void:
 	price_label.text = str("Current Price: \n", snapped(price,0.01), " score")
+	relic.update_label()
